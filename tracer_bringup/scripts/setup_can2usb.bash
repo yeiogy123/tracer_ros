c95ffcff385ecac1bf2d787a449ec613ee94b576
user <ginosjchang@gmail.com>
2024-04-17 12:32:30 +0800

    custom

--- tracer_bringup/scripts/setup_can2usb.bash
+++ tracer_bringup/scripts/setup_can2usb.bash
@@ -3,8 +3,13 @@
 # enable kernel module: gs_usb
 sudo modprobe gs_usb
 
-# bring up can interface
-sudo ip link set can0 up type can bitrate 500000
-
 # install can utils
 sudo apt install -y can-utils
+
+# udev rule for bring up can interface
+sudo cp tracer.rules
+sudo cp `rospack find tracer_bringup`/scripts/tracer.rules /etc/udev/rules.d
+
+sudo service udev reload
+sudo service udev restart
+sudo udevadm control --reload && sudo udevadm trigger
