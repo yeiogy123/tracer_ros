40ad84de940a86644c85fedcf8e66dd68302797f
user <ginosjchang@gmail.com>
2024-08-05 13:02:31 +0800

    for velodyne

--- tracer_bringup/scripts/setup_can2usb.bash
+++ tracer_bringup/scripts/setup_can2usb.bash
@@ -9,6 +9,9 @@ sudo apt install -y can-utils
 # udev rule for bring up can interface
 sudo cp `rospack find tracer_bringup`/scripts/tracer.rules /etc/udev/rules.d
 
+# Remap velodyne scan
+sudo cp `rospack find tracer_bringup`/scripts/laserscan_nodelet.launch `rospack find velodyne_pointcloud`/launch/laserscan_nodelet.launch 
+
 sudo service udev reload
 sudo service udev restart
 sudo udevadm control --reload && sudo udevadm trigger
